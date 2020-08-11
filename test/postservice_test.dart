4a79653237e9d2f32a4b3048949b6974c7efabd7
samuelematias <hi@samuelematias.com>
2020-08-11 17:17:50 -0300

    feat(tests): update tests with abstract(interface)

--- test/postservice_test.dart
+++ test/postservice_test.dart
@@ -1,12 +1,19 @@
 import 'package:abstraction_unit_test_get_it/locator.dart';
-import 'package:abstraction_unit_test_get_it/services/localstorage_service.dart';
+import 'package:abstraction_unit_test_get_it/services/api/api.dart';
 import 'package:abstraction_unit_test_get_it/services/post_service.dart';
+import 'package:abstraction_unit_test_get_it/services/storage/storage_service.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockStorageService extends Mock implements StorageService {}
+
+class MockApi extends Mock implements Api {}
 
 void main() {
   group('PostService Test | ', () {
     setUpAll(() {
       setupLocator();
+      locator.allowReassignment = true;
     });
 
     test('Constructing Service should find correct dependencies', () {
@@ -14,16 +21,50 @@ void main() {
       expect(postService != null, true);
     });
 
+    // After implementing the abstract class (interface)
     test('Given postId 1, should call localStorageService with 1', () async {
-      var localStorageService = LocalStorageService();
-      locator.registerSingleton(localStorageService);
+      var mockStorageService = MockStorageService();
+      locator.registerSingleton<StorageService>(mockStorageService);
+
+      var mockApi = MockApi();
+      // return fake subs
+      when(mockApi.likePost(1)).thenAnswer((_) => Future.value(true));
+      locator.registerSingleton<Api>(mockApi);
 
       var postService = PostService();
       await postService.likePost(1);
-      // expect(localStorageService);
+      verify(mockStorageService.likePost(1));
+    });
 
-      // We can't even check if the function was called!
-      // expect(postService.likePost(1)) // ???
+    // Before implementing the abstract class (interface)
+    // test('Given postId 1, should call localStorageService with 1', () async {
+    //   var localStorageService = LocalStorageService();
+    //   locator.registerSingleton(localStorageService);
+
+    //   var postService = PostService();
+    //   await postService.likePost(1);
+    //   // expect(localStorageService);
+
+    //   // We can't even check if the function was called!
+    //   // expect(postService.likePost(1)) // ???
+    // });
+
+    test('Given like fails, should rever localike', () async {
+      var mockStorageService = MockStorageService();
+      // Like was successful locally
+      when(mockStorageService.likePost(1))
+          .thenAnswer((_) => Future.value(true));
+      locator.registerSingleton<StorageService>(mockStorageService);
+
+      var mockApi = MockApi();
+      // Like was successful locally
+      when(mockApi.likePost(1)).thenAnswer((_) => Future.value(false));
+      locator.registerSingleton<Api>(mockApi);
+
+      var postService = PostService();
+      await postService.likePost(1);
+      // Confirm the sstorage like was called with unlike
+      verify(mockStorageService.likePost(1, unlike: true));
     });
   });
 }
